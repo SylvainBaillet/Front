fa81be9c8c3932063a40757a4ab302972e471400
SylvainBaillet <45996506+SylvainBaillet@users.noreply.github.com>
2019-01-31 16:46:13 +0100

    tp_Azzaro_JSHorloge_31012019

---
+++ JS/Exo/JS/03_exo.js
@@ -0,0 +1,16 @@
+
+var a = 3;
+var b = 2;
+    
+    function multiplie(x * 8){
+        return 3 * x;
+    }
+
+    function afficher(){
+        alert(multiplie(a));
+        alert(multiplie(b));
+        alert(multiplie());
+    }
+        
+
+
---
+++ JS/Exo/JS/05_exo.js
@@ -0,0 +1,2 @@
+var compteurMinutes
+var compteurHeures
